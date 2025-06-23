3c980a6e1bd1fc92c81ff981a1da8ebf038f46f1
Amanda de Mello <110688053+AMelloDev@users.noreply.github.com>
2025-06-23 20:34:19 -0300

    Add files via upload

---
+++ TAD_Tree.c
@@ -0,0 +1,187 @@
+
+#include <stdio.h>
+
+
+typedef struct no_arvore{
+    int chave;
+    struct no_arvore *esq;
+    struct no_arvore *dir;
+
+}no_arvore;
+
+typedef struct {
+    no_arvore * raiz;
+}Arvore;
+
+Arvore* cria_arvore(){
+    Arvore* arv = malloc(sizeof(Arvore));
+    arv-> raiz = NULL;
+    return arv;
+}
+
+int inserir_arvore(Arvore * arv, int chave){
+    no_arvore * novo = malloc(sizeof(no_arvore));
+    no_arvore * aux, anterior;
+    novo->chave = chave;
+    novo -> esq = NULL;
+    novo -> dir = NULL;
+    if(arv->raiz==NULL){
+        arv-> raiz = novo;
+        return chave;
+    }
+    aux=aux -> raiz;
+    anterior = aux;
+    
+    while(aux!= NULL){
+        if(aux-> chave > chave){
+            anterior = aux;
+            aux = aux-> esq;
+        }else {
+            if(aux->chave < chave){
+                anterior= aux;
+                aux = aux ->dir;
+            }else{
+                return 0; //chave já existe
+            }
+        }
+    }
+    if(anterior-> chave > chave){
+        anterior->esq = novo
+    }else{
+        anterior->dir = novo;
+    }
+    return chave;
+}
+
+no_arvore* buscar_arvore(Arvore * arv, int chave){
+    no_arvore * aux = arv -> raiz;
+    
+    while(aux!=NULL ){
+        if(chave == aux-> chave){
+             return aux;
+        }else 
+        if(chave< aux-> chave){
+            aux= aux->esq;
+        }else{
+                aux= aux->dir;
+            
+        }
+    }
+    return aux;
+}
+void pre_ordem(no_arvore* no){
+    if(no == NULL){
+        return;
+    }
+    printf("%d", no ->chave);
+    pre_ordem(no->esq);
+    pre_ordem(no->dir);
+}
+void in_ordem(no_arvore*no){
+    if(no == NULL){
+        return;
+    }
+    pre_ordem(no->esq);
+    printf("%d", no ->chave);
+    pre_ordem(no->dir);
+}
+
+void pos_ordem(no_arvore* no){
+    if(no == NULL){
+        return;
+    }
+    pre_ordem(no->esq);
+    
+    pre_ordem(no->dir);
+    printf("%d", no ->chave);
+}
+void percorrer_pre_orden(no_arvore *arv){
+    pre_ordem(arv-> raiz);
+}
+void percorrer_in_ordem(no_arvore *arv){
+    in_ordem(arv-> raiz);
+}
+void percorrer_pos_ordem(no_arvore *arv){
+    pos_ordem(arv-> raiz);
+}
+
+void remover_arvore(Arvore *arv, int chave){
+     no_arvore * aux , *pai = NULL , *filho;
+     no_arvore * novo, *ant;
+     aux = arv -> raiz;
+    
+    while(aux!=NULL ){
+        if(chave == aux-> chave){
+            break;
+            pai =aux;
+            
+        }else if(chave< aux-> chave){
+            aux= aux->esq;
+        }else{
+     
+                aux= aux->dir;
+        } 
+        
+        if(aux == NULL){
+            return 0;
+        }
+    }
+    
+    if(aux-> esq ==NULL && aux->dir==NULL){
+        if(aux==pai->esq)
+            pai->esq =NULL;
+        else
+            pai ->dir =NULL;
+        free(aux);
+        return 1;
+    }
+
+    if(auxn -> esq == NULL || aux -> dir == NULL){
+        if(aux -> esq == NULL){
+            filho = aux -> dir;
+        }else{
+            filho = aux-> esq;
+        }if(aux==pai->esq)
+            pai->esq =filho;
+        else
+            pai ->dir =filho;
+        free(aux);
+        return 1;
+    }else{
+        novo = aux-> esq;
+        ant = aux;
+        
+        while(novo ->dir != NULL){
+            ant = novo;
+            novo = novo -> dir;
+        }
+        aux = chave = novo-> chave;
+        ant -> dir = NULL;
+        free(novo);
+        return 1;
+    }
+    
+}
+
+void destruir_no(no_arvore* no) {
+    if (no == NULL) return;
+    destruir_no(no->esq);
+    destruir_no(no->dir);
+    free(no);
+}
+
+
+void destruir_arvore(Arvore *arv){
+    destruir_no(arv->raiz);
+    free(arv);
+    
+}
+
+
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
